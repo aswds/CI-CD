f4534c36047d57b61432433e9f1a5907c9204cc6
aswds <sasha.iilllnitskiy@gmail.com>
2023-03-29 14:31:41 +0300

    Writing intersection data into same.txt and symmetric_difference data to diff.txt

--- main.py
+++ main.py
@@ -3,8 +3,16 @@ with open('file1.txt', 'r') as file1, open('file2.txt', 'r') as file2:
     file1_lines = file1.readlines()
     file2_lines = file2.readlines()
 
-#Перевіряємо чи у сетів є перетинів у словах
+# Перевіряємо чи у сетів є перетинів у словах
 same_lines = set(file1_lines).intersection(file2_lines)
 
-#Перевіряємо чи у сетів немає перетинів у словах
+# Перевіряємо чи у сетів немає перетинів у словах
 diff_lines = set(file1_lines).symmetric_difference(file2_lines)
+
+
+with open('same.txt', 'w') as same_file, open('diff.txt', 'w') as diff_file:
+    for line in same_lines:
+        same_file.write(line)
+
+    for line in diff_lines:
+        diff_file.write(line)
